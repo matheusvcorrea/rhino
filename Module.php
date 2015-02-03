490249c11642ebc6fb5bf0fd6db827caf0efca24
Matheus Correa <matheuscorrea@live.com>
2015-02-03 02:17:48 -0200

    Autoloader Config

--- Module.php
+++ Module.php
@@ -7,7 +7,19 @@ use Zend\ModuleManager\Feature\ConfigProviderInterface;
 
 class Module implements AutoloaderProviderInterface, ConfigProviderInterface
 {
-   public function getAutoloaderConfig() { }
+   public function getAutoloaderConfig()
+   {
+	   	return array(
+	   		'Zend\Loader\ClassMapAutoloader' => array(
+	   			__DIR__ . '/autoload_classmap.php',
+	   		),
+	   		'Zend\Loader\StandardAutoloader' => array(
+	   			'namespaces' => array(
+	   				__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+	   			),
+	   		),
+	   	);
+   }
 
    public function getConfig()
    {
