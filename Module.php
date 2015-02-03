8a13657628ebd89ee714977636be941b486c61ba
Matheus Correa <matheuscorrea@live.com>
2015-02-02 22:34:15 -0200

    Inicial Commit

---
+++ Module.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Album;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface
+{
+   public function getAutoloaderConfig() { }
+
+   public function getConfig()
+   {
+       return include __DIR__ . '/config/module.config.php';
+   }
+}
