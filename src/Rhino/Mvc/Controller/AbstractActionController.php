2440f347edc582c83de4e36a74ac1d7727efdd99
Matheus Correa <matheuscorrea@live.com>
2015-02-03 02:08:03 -0200

    Create Abstract Action Controller class

---
+++ src/Rhino/Mvc/Controller/AbstractActionController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Rhino\Mvc\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController as AbstractAction;
+use Zend\Stdlib\RequestInterface;
+use Zend\Stdlib\ResponseInterface;
+use Zend\Mvc\MvcEvent;
+
+/**
+ * Base Abstract Action Controller
+ */
+abstract class AbstractActionController extends AbstractAction
+{
+	
+}
